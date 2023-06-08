a27b509ccba68044f379132d7861872ecf768516
alekskorolev3 <alekskorolev3@gmail.com>
2023-06-08 13:33:11 +0300

    attempt to fix iframe

--- src/App.js
+++ src/App.js
@@ -17,6 +17,10 @@ function App() {
   return (
     <div className="App">
         <button onClick={window.payment}>Close modal</button>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/MzO-0IYkZMU" title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen></iframe>
     </div>
   );
 }
