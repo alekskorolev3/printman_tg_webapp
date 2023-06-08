3707c1a81039a120462a860735f184d132319c88
alekskorolev3 <alekskorolev3@gmail.com>
2023-06-08 16:35:06 +0300

    testing some issue6

--- src/App.js
+++ src/App.js
@@ -2,22 +2,9 @@ import './App.css';
 
 function App() {
 
-    // useEffect(() => {
-    //     tg.ready()
-    // }, [])
-
-    // const onClose = () => {
-    //     tg.close()
-    // }
-
   return (
     <div className="App">
-        {/*<a onClick={onClose} target="_parent">open payment</a>*/}
-        {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/MzO-0IYkZMU" title="YouTube video player"*/}
-        {/*        frameBorder="0"*/}
-        {/*        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"*/}
-        {/*        allowFullScreen></iframe>*/}
-
+      {console.log(window)}
     </div>
   );
 }
