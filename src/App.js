c6a063880fe260b62927bbc205299a72fc2891ae
alekskorolev3 <alekskorolev3@gmail.com>
2023-06-08 13:56:08 +0300

    attempt to fix iframe 5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-        <a onClick={onClose} target="_parent">open payment</a>
+        {/*<a onClick={onClose} target="_parent">open payment</a>*/}
         {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/MzO-0IYkZMU" title="YouTube video player"*/}
         {/*        frameBorder="0"*/}
         {/*        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"*/}
