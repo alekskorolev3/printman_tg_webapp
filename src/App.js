97ea92c3d5fa59e61c7fe59fa4d8d6a13fb8e6b4
alekskorolev3 <alekskorolev3@gmail.com>
2023-06-08 13:47:59 +0300

    attempt to fix iframe 2

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ function App() {
         tg.ready()
     }, [])
 
-    // const onClose = () => {
-    //     tg.close()
-    // }
+    const onClose = () => {
+        tg.close()
+    }
+
 
 
   return (
     <div className="App">
-        <button onClick={window.payment}>Close modal</button>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/MzO-0IYkZMU" title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen></iframe>
+        <a onClick={window.payment} target="_parent">open payment</a>
+        {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/MzO-0IYkZMU" title="YouTube video player"*/}
+        {/*        frameBorder="0"*/}
+        {/*        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"*/}
+        {/*        allowFullScreen></iframe>*/}
+
     </div>
   );
 }
